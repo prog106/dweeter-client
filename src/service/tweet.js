54e69785f57d84db19f845463f2dc08bb16ee827
prog106 <prog106@gmail.com>
2021-06-25 19:02:52 +0900

    Dweet!

--- src/service/tweet.js
+++ src/service/tweet.js
@@ -1,43 +1,35 @@
 export default class TweetService {
-  tweets = [
-    {
-      id: 1,
-      text: '드림코딩에서 강의 들으면 너무 좋으다',
-      createdAt: '2021-05-09T04:20:57.000Z',
-      name: 'Bob',
-      username: 'bob',
-      url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
-    },
-  ];
-
-  async getTweets(username) {
-    return username
-      ? this.tweets.filter((tweet) => tweet.username === username)
-      : this.tweets;
-  }
-
-  async postTweet(text) {
-    const tweet = {
-      id: Date.now(),
-      createdAt: new Date(),
-      name: 'Ellie',
-      username: 'ellie',
-      text,
-    };
-    this.tweets.push(tweet);
-    return tweet;
-  }
-
-  async deleteTweet(tweetId) {
-    this.tweets = this.tweets.filter((tweet) => tweet.id !== tweetId);
-  }
-
-  async updateTweet(tweetId, text) {
-    const tweet = this.tweets.find((tweet) => tweet.id === tweetId);
-    if (!tweet) {
-      throw new Error('tweet not found!');
+    constructor(http) {
+        this.http = http;
     }
-    tweet.text = text;
-    return tweet;
-  }
+    async getTweets(username) {
+        const query = username ? `?username=${username}` : ``;
+        return this.http.fetch(`/tweets${query}`, {
+            method: "GET",
+        });
+    }
+
+    async postTweet(text) {
+        return this.http.fetch(`/tweets`, {
+            method: "POST",
+            body: JSON.stringify({
+                text: text,
+                username: "ellie",
+                name: "ellie",
+            }),
+        });
+    }
+
+    async deleteTweet(tweetId) {
+        return this.http.fetch(`/tweets/${tweetId}`, {
+            method: "DELETE",
+        });
+    }
+
+    async updateTweet(tweetId, text) {
+        return this.http.fetch(`/tweets/${tweetId}`, {
+            method: "PUT",
+            body: JSON.stringify({ text: text }),
+        });
+    }
 }
